3f2b995789c82e29d3805783961531e1468e4be8
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2022-05-04 10:25:45 +0200

    feat: first page

--- src/components/calculator/calculator.tsx
+++ src/components/calculator/calculator.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-export default function calculator() {
+export default function Calculator() {
   return (
     <Grid>
-    <Typography>coucou</Typography>
-    <Button>Button</Button>
-  </Grid>
+      <Typography>Bonjour je suis la calculette</Typography>
+    </Grid>
   )
 }
