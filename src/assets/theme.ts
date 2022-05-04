498e4d17295d72c611877c7e6f4e06c63aa1e151
BastianPEIRE <bastian.peire@gmail.com>
2022-05-04 11:01:14 +0200

    theme mui colors + font

--- src/assets/theme.ts
+++ src/assets/theme.ts
@@ -2,106 +2,36 @@ import { ThemeOptions } from '@mui/material'
 import { TypographyOptions } from '@mui/material/styles/createTypography'
 
 // Create a theme instance.
-const colors = {
-  textBlack: '#1A1D1F',
-  textWhite: 'white',
-  textLight: '#747474',
-  gradientStart: '#206A9B',
-  gradientEnd: '#000714',
-  blue: '#1F6898',
-  bgGray: '#F4F4F4',
-  bgBlue: '#F3F7FA',
-  red: '#FF6A55',
-  orange: '#FFB20F',
-  orangeLight: '#FFAB76',
-  green: '#74C99A',
-}
+const font = "'Montserrat', sans-serif";
+
+
 const palette = {
+  type: 'light',
   primary: {
-    main: colors.blue,
-    dark: colors.blue,
-    light: `${colors.blue}5c`,
-    contrastText: '#fff',
+    main: '#0060ff',
   },
   secondary: {
-    main: colors.textLight,
-    light: `${colors.textLight}5c`,
-    dark: colors.textBlack,
-    contrastText: colors.textBlack,
-  },
-  error: {
-    main: colors.red,
-    light: colors.red,
-    dark: colors.red,
-    contrastText: '#FFF',
-  },
-  warning: {
-    main: colors.orange,
-    light: colors.orange,
-    dark: colors.orange,
-    contrastText: '#FFF',
+    main: '#ffc300',
+    contrastText: '#ffffff',
   },
   info: {
-    main: '#000',
-    light: '#000',
-    dark: '#000',
-    contrastText: '#FFF',
-  },
-  success: { main: colors.green, light: colors.green, dark: colors.green, contrastText: colors.textBlack },
-  background: {
-    paper: colors.bgGray,
-    default: colors.bgGray,
-  },
-  text: {
-    primary: colors.textBlack,
-    secondary: colors.textBlack,
-    disabled: colors.textLight,
-  },
-  gradient: {
-    main: colors.gradientStart,
-    dark: colors.gradientEnd,
-  },
-  backgroundBlue: { main: colors.bgBlue },
-  action: {
-    hover: colors.bgGray,
+    main: '#00AEFF',
+    contrastText: '#ffffff',
   },
 }
 
 const typography: TypographyOptions = {
-    fontFamily: 'Inter',
-    h1: {
-      fontSize: '28px',
-      fontWeight: '600',
-      lineHeight: '48px',
-      letterSpacing: '-0.5px',
-    },
-    h2: {
-      fontSize: '20px',
-      fontWeight: '600',
-      lineHeight: '32px',
-      letterSpacing: '-0.5px',
-    },
-    h3: {
-      fontSize: '15px',
-      fontWeight: '600',
-      lineHeight: '24px',
-      letterSpacing: '-0.15px',
-    },
-  }
+  fontFamily: font,
+  fontSize: 14,
+  fontWeightLight: 400,
+  fontWeightRegular: 600,
+  fontWeightMedium: 700,
+}
 
 const theme = {
     typography,
     palette: { ...palette },
-    colors: { ...colors },
-    components: {
-      MuiGrid: {
-        variants: [],
-      },
-      MuiTypography: {
-        variants: [],
-      },
-    },
-  }
+}
 
 const typeTheme = theme as ThemeOptions
 
--- src/components/calculator/calculator.tsx
+++ src/components/calculator/calculator.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useMemo } from 'react';
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 export default function calculator() {
   return (
-    <Grid>
-    <Typography>coucou</Typography>
-    <Button>Button</Button>
-  </Grid>
+      <Grid>
+        <Typography>Calcualtor</Typography>
+        <Button variant='contained'>Button</Button>
+        <Button variant='outlined'>Outlined</Button>
+        <Button variant='text'>Button</Button>
+        <Button color='secondary'>Secondary</Button>
+        <Button color='secondary' variant='contained'>Button</Button>
+        <Button color='info' variant='outlined'>Outlined</Button>
+        <Button color='info' variant='contained'>Outlined</Button>
+      </Grid>
   )
 }
