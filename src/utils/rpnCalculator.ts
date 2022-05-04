abf6ed3f359ee245b6f552cafed81b5513385eb0
charlescosneau <charles@maplab.green>
2022-05-04 15:35:27 +0200

    resolve save

--- src/utils/rpnCalculator.ts
+++ src/utils/rpnCalculator.ts
@@ -1,3 +1,40 @@
-export default function rpnCalculator() {
-  return ()
+import { useState } from 'react';
+
+const [operator1, setOperator1] = useState<(number | string)>();
+const [operator2, setOperator2] = useState<(number | string)>();
+
+function calculate(operation: (number | string)[]) {
+  operation.forEach((item: number | string, index: number) => {
+    if (typeof(item) === 'string') {
+      setOperator1(operation[index - 2]);
+      setOperator2(operation[index - 1]);
+      if (item === '+') {
+        addNumbers([Number(operator1), Number(operator2)]);
+      } else if (item === '-') {
+        subtractNumbers([Number(operator1), Number(operator2)]);
+      } else if (item === '*') {
+        multiplyNumbers([Number(operator1), Number(operator2)]);
+      } else if (item === '/') {
+        divideNumbers([Number(operator1), Number(operator2)]);
+      }
+    }
+  })
+}
+
+function multiplyNumbers(operators: [number, number]): number {
+  return operators[0] * operators[1];
 };
+
+function divideNumbers(operators: [number, number]): number {
+  return operators[0] / operators[1];
+};
+
+function addNumbers(operators: [number, number]): number {
+  return operators[0] + operators[1];
+};
+
+function subtractNumbers(operators: [number, number]): number {
+  return operators[0] - operators[1];
+};
+
+export {};
