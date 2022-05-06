4cd66ae27d20fff73ee314d0bf957963871b69d8
charlescosneau <charles@maplab.green>
2022-05-06 14:34:34 +0200

    add negate - float

--- src/utils/rpnInputCalculator.ts
+++ src/utils/rpnInputCalculator.ts
@@ -1,39 +1,46 @@
 export const additionCalculation = (firstNumber: number, secondNumber: number) => {
-  return firstNumber + secondNumber;
+  return (firstNumber) + (secondNumber);
 };
 
 export const substractionCalculation = (firstNumber: number, secondNumber: number) => {
-  return firstNumber - secondNumber;
+  return (firstNumber) - (secondNumber);
 };
 
 export const multiplicationCalculation = (firstNumber: number, secondNumber: number) => {
-  return firstNumber * secondNumber;
+  return (firstNumber) * (secondNumber);
 };
 
 export const divisionCalculation = (firstNumber: number, secondNumber: number) => {
-  return firstNumber / secondNumber;
+  return (firstNumber) / (secondNumber);
 };
 
 export default function calculate(operation: (number | string)[]): number | string {
   let result: number | string = 0;
-  while (operation.length > 1) {
-    operation.forEach((caracter: number | string, index: number) => {
-      if (caracter === '+') {
-        result = additionCalculation(Number(operation[index - 2]), Number(operation[index - 1]));
-      } else if (caracter === '-') {
-        result = substractionCalculation(Number(operation[index - 2]), Number(operation[index - 1]));
-      } else if (caracter === '*') {
-        result = multiplicationCalculation(Number(operation[index - 2]), Number(operation[index - 1]));
-      } else if (caracter === '/') {
-        result = divisionCalculation(Number(operation[index - 2]), Number(operation[index - 1]));
-      } else if (+caracter) {
-        return;
-      } else {
-        result = "Mauvaise operation"
+  for (let i = 0; i < operation.length; i++) {
+    if (operation.length > 1) {
+      if (!+operation[i]) {
+        if (operation[i] === '+') {
+          result = additionCalculation(Number(operation[i - 2]), Number(operation[i - 1]));
+        } else if (operation[i] === '-') {
+          result = substractionCalculation(Number(operation[i - 2]), Number(operation[i - 1]));
+        } else if (operation[i] === '*') {
+          result = multiplicationCalculation(Number(operation[i - 2]), Number(operation[i - 1]));
+        } else if (operation[i] === '/') {
+          result = divisionCalculation(Number(operation[i - 2]), Number(operation[i - 1]));
+        } else {
+          result = "Mauvaise operation";
+        }
+        operation.splice(i - 2, 3);
+        operation.splice(i -2, 0, result);
+        i = 0;
       }
-      operation.splice(index - 2, 3);
-      operation.splice(index -2, 0, result);
-    });
+    } else {
+      return operation[0];
+    }
+  }
+  console.log(result);
+  if (!result) {
+    result = "Mauvaise operation"; 
   }
   return result;
 }
